1137e1e2d6ef1201d6c8306b4286adfa2ed8988e
Avishek-Ad <adhikariavishek03@gmail.com>
2025-11-28 16:08:29 +0545

    Remove duplicate setting of DJANGO_SETTINGS_MODULE in asgi.py

--- chess_core/asgi.py
+++ chess_core/asgi.py
@@ -1,11 +1,10 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess_core.settings')
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 # using custom middleware cause the custom on sucks or i didnot know how to use it
 from game.middleware import TokenAuthMiddlewareStack
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess_core.settings')
 
 django_asgi_application = get_asgi_application()
 
